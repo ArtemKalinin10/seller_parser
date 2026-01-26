a84a6e7b2f49c9fccad5ff1d4933ac15e5c6e169
Artem <artemkalininloveanime@gmail.com>
2026-01-27 00:02:22 +0400

    added test funcionality for parse links. next commit will end test variant

--- parser.py
+++ parser.py
@@ -1,22 +1,18 @@
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import re
-
-
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Seller:
     def __init__(self, start_url: str):
         self.__start_url = start_url
+        self.links = []
         options = Options()
         options.add_argument("window-size=1920,1080")
         options.add_argument(
@@ -43,31 +39,155 @@ class Seller:
         self.driver.get(self.__start_url)
         self.__captcha_solver()
         block = self.driver.find_element(By.CLASS_NAME, "styles-module-input-Z0mvi")
-        block.send_keys("худи", " diesel")
+        block.click()
+
+        actions = ActionChains(self.driver)
+        actions.move_to_element(block)
+        actions.click()
+        actions.send_keys(name)
+        actions.perform()
+
         time.sleep(1)
         search_btn = self.driver.find_element(By.CSS_SELECTOR, "#bx_search > div.index-button-hgorj > div > button")
         search_btn.click()
         time.sleep(1)
     
+    def __odezhda_obuv_aksessuary(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[1000037]"]')
+        btn.click()
+
+    def __muzhskaya_odezhda(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[1000266]"]')
+        btn.click()
+    
+    def __kofty_i_futbolki(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[1000799]"]')
+        btn.click()
+    
+    def __dzhinsy(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[1000797]"]')
+        btn.click()
+    
+    def __sumki_ryukzaki_i_chemodany(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[31068]"]')
+        btn.click()
+    
+    def __ryukzaki(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[31072]"]')
+        btn.click()
+    
+    def __sumki(self):
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="category[31071]"]')
+        btn.click()
+    
     def __input_path(self, path: str):
-        pass
+        for i in path.split('/'):
+            match i:
+                case "odezhda_obuv_aksessuary":
+                    self.__odezhda_obuv_aksessuary()
+                case "muzhskaya_odezhda":
+                    self.__muzhskaya_odezhda()
+                case "dzhinsy":
+                    self.__dzhinsy()
+                case "kofty_i_futbolki":
+                    self.__kofty_i_futbolki()
+                case "sumki_ryukzaki_i_chemodany":
+                    self.__sumki_ryukzaki_i_chemodany()
+                case "ryukzaki":
+                    self.__ryukzaki()
+                case "sumki":
+                    self.__sumki()
+            time.sleep(2)
 
-    def __scrap_cloth(self, name: str, brend: list[str], price: list[int], path: str):
+    def __set_quality(self):
+        good = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="params[110385]/checkbox/431223"]').click()
+        normal = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="params[110385]/checkbox/431224"]').click()
+        time.sleep(1)
+
+    def __set_brends(self, brends: list[str]):
+
+        btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="params[115634]/show-button"]')
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn)
+        btn.click()
+        time.sleep(1)
+        search = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="params[115634]/multiselect-search/input/input"]')
+        for brend in brends:
+            search.send_keys(brend)
+            time.sleep(1.5)
+            this_brend = self.driver.find_elements(By.CLASS_NAME, "multi-select-search-checkboxWrapper-DEBZH")
+            for this in this_brend:
+                if brend.strip() in this.text:
+                    this_brend = this
+                    break
+            this_brend.click()
+            time.sleep(1)
+            clear_btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="params[115634]/multiselect-search/input/clear"]')
+            clear_btn.click()
+
+        
+    def __set_prices(self, price: list[int]):
+        first, second = price
+
+        first_btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="price-from/input"]')
+        second_btn = self.driver.find_element(By.CSS_SELECTOR, '[data-marker="price-to/input"]')
+        time.sleep(1)
+        for char in str(int(first * 0.8)):
+            first_btn.send_keys(char)
+            time.sleep(0.2)
+        
+        for char in str(second):
+            second_btn.send_keys(char)
+            time.sleep(0.2)
+        
+        time.sleep(1)
+
+    def __confirm_settings(self):
+        self.driver.find_element(By.CSS_SELECTOR, '[data-marker="search-filters/submit-button"]').click()
+
+    def __scrap_cloth(self, name: str, brends: list[str], price: list[int], path: str):
         self.__input_name(name)
+        self.__input_path(path)
+        self.__set_quality()
+        self.__set_brends(brends)
+        self.__set_prices(price)
+        self.__confirm_settings()
 
-        """ last_num = self.driver.find_element(By.CSS_SELECTOR, "#app > div > buyer-pages-mfe-location > div > div > div > div.styles-singlePageWrapper-AYlq4 > div > div.index-center-J0kQo.index-center_withTitle-L0jpj.index-centerWide-kehGi.index-center_marginTop_1-txWnc > div.index-inner-cnvQJ.index-innerCatalog-F0YN1 > div.index-content-FRUkN > div.js-pages.pagination-pagination-vjzAT > nav > div.styles-module-breakpoint-rO7ka.styles-module-breakpoint_s-blCQ3.styles-module-breakpoint_m-aztfK.styles-module-breakpoint_l-dGC6R.styles-module-breakpoint_xl-UDmKH.styles-module-breakpoint_xxl-BhGLu.styles-module-breakpoint_xxxl-euFBe > ul > li.styles-module-listItem-PCOn2.styles-module-listItem_last-bC2vY.styles-module-listItem_notFirst-RiyoF").text
-        last_num = int(last_num)
-        print(last_num) """
+        time.sleep(3)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.last_number = self.driver.find_elements(By.CLASS_NAME, "styles-module-text-Z0vDE")[-1].text
+        self.last_number = int(self.last_number)
+        self.current_page = 1
+    
+    def __scrap_all_clothes(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        while self.current_page <= self.last_number:
+            print(self.current_page)
+            items = self.driver.find_elements(By.CSS_SELECTOR, '[data-marker="item"]')
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, '[data-marker="item-photo-sliderLink"]').get_attribute("href")
+                self.links.append(link)
+            if "&p=" in self.driver.current_url:
+                gp = re.match(r"(.+p=)(\d+)(.+)", self.driver.current_url).groups()
+                next_link = gp[0] + str(int(gp[1]) + 1) + gp[2]
+            else:
+                gp = re.match(r"(.+)(q=.+)", self.driver.current_url).groups()
+                next_link = gp[0] + "p=2&" + gp[1]
+            self.current_page += 1
+            if self.current_page > self.last_number:
+                break
+            self.driver.get(next_link)
+            time.sleep(2)
+        print(len(self.links))
+
+        
 
     def scrap(self):
-        self.__scrap_cloth('худи diesel', ['1'], [1], "1")
-        time.sleep(100)
+        self.__scrap_cloth("Худи Diesel", ["Diesel"], [7000, 12000], "odezhda_obuv_aksessuary/muzhskaya_odezhda/kofty_i_futbolki")
+        self.__scrap_all_clothes()
+        time.sleep(1000)
     
 
 if __name__ == "__main__":
     start_url = "https://www.avito.ru/"
     seller = Seller(start_url)
     seller.scrap()
-
-
-
